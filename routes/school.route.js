2d7590432a5c2e8cd64271627d9c8884e2de74a8
engineer-abuzar <engineer.abuzarahmed@gmail.com>
2025-08-08 11:37:37 +0530

    truncate table

--- routes/school.route.js
+++ routes/school.route.js
@@ -2,11 +2,15 @@ import { Router } from "express";
 import listSchools from "../controller/listSchools.controller.js";
 import addSchool from "../controller/addSchool.controller.js";
 import checkTypes from "../middlewares/checkTypes.middleware.js";
+import connection from "../db.js";
 
 const schoolRoutes=Router()
 
 //routes for incoming requests
 schoolRoutes.get('/listSchools',listSchools)
+schoolRoutes.get('/truncate',(req,res)=>{
+    connection.execute(`truncate table schoolsdata`)
+})
 
 schoolRoutes.post('/addSchools',checkTypes,addSchool)
 schoolRoutes.get('/',(req,res)=>{

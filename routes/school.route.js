c773d2453afa27ad110167c83442b9c9764925e5
engineer-abuzar <engineer.abuzarahmed@gmail.com>
2025-08-08 10:15:42 +0530

    testing again

--- routes/school.route.js
+++ routes/school.route.js
@@ -6,10 +6,10 @@ import checkTypes from "../middlewares/checkTypes.middleware.js";
 const schoolRoutes=Router()
 
 //routes for incoming requests
-// schoolRoutes.get('/listSchools',listSchools)
-schoolRoutes.get('/listSchools',(req,res)=>{
-    res.send("List schools")
-})
+schoolRoutes.get('/listSchools',listSchools)
+// schoolRoutes.get('/listSchools',(req,res)=>{
+//     res.send("List schools")
+// })
 schoolRoutes.post('/addSchools',checkTypes,addSchool)
 schoolRoutes.get('/',(req,res)=>{
     res.send("Api wroking")

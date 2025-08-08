cf67a40a703b0a9e5a785d8294d44d8245562f61
engineer-abuzar <engineer.abuzarahmed@gmail.com>
2025-08-08 11:12:00 +0530

    db configured

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ import mysql from 'mysql2/promise'
 
 const connection=mysql.createPool({
     user:"root"
-,port:"3306",
-    host:"mysql://root:ZlyUsTOMKsXudyFAokYxSDEPwatqYyZl@yamabiko.proxy.rlwy.net:26324/railway",
+,port:"26324",
+    host:"yamabiko.proxy.rlwy.net",
     database:"railway",
     password:"ZlyUsTOMKsXudyFAokYxSDEPwatqYyZl",
 }
